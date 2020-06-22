67ed319ef402ece2c093592b0fb5352cdbe085c0
DESKTOP-60MQ997\vuanh <vuanh.nguyen92@outlook.com>
2020-06-23 01:27:18 +0900

    Fix output flip to new Squarelotron, fix turn to same Squarelotron

--- Squarelotron/Squarelotron.java
+++ Squarelotron/Squarelotron.java
@@ -18,98 +18,106 @@ public class Squarelotron {
         this.matrix = matrixT;
     }
 
-    public int[][] DiagonalFlip(int ring) {
+    public Squarelotron DiagonalFlip(int ring) {
         System.out.println("Flipping matrix diagonally...");
-        int[][] matrixFlipped = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                matrixFlipped[row][col] = matrix[row][col];
-            }
-        }
+        Squarelotron newMatrix = new Squarelotron(size);
+        //int[][] matrixFlipped = new int[size][size];
+        //for (int row = 0; row < size; row++) {
+            //for (int col = 0; col < size; col++) {
+                //matrixFlipped[row][col] = matrix[row][col];
+            //}
+        //}
 
         int sizeR = size-1;
         for (int i = ring -1; i < (size-(ring-1)); i++) {
-            matrixFlipped[ring-1][i] = matrix[i][ring-1]; //flipping row 1 & col 1
-            matrixFlipped[i][ring-1] = matrix[ring-1][i]; //flipping col 1 & row 1
-            matrixFlipped[sizeR-(ring-1)][i] = matrix[i][sizeR-(ring-1)]; //flipping row n & col n
-            matrixFlipped[i][sizeR-(ring-1)] = matrix[sizeR-(ring-1)][i]; //flipping col n & row n
+            // matrixFlipped[ring-1][i] = matrix[i][ring-1]; //flipping row 1 & col 1
+            // matrixFlipped[i][ring-1] = matrix[ring-1][i]; //flipping col 1 & row 1
+            // matrixFlipped[sizeR-(ring-1)][i] = matrix[i][sizeR-(ring-1)]; //flipping row n & col n
+            // matrixFlipped[i][sizeR-(ring-1)] = matrix[sizeR-(ring-1)][i]; //flipping col n & row n
+            newMatrix.matrix[ring-1][i] = matrix[i][ring-1]; //flipping row 1 & col 1
+            newMatrix.matrix[i][ring-1] = matrix[ring-1][i]; //flipping col 1 & row 1
+            newMatrix.matrix[sizeR-(ring-1)][i] = matrix[i][sizeR-(ring-1)]; //flipping row n & col n
+            newMatrix.matrix[i][sizeR-(ring-1)] = matrix[sizeR-(ring-1)][i]; //flipping col n & row n
         }
 
-        return matrixFlipped;
+        return newMatrix;
     }
 
-    public int[][] UpDownFlip(int ring) {
+    public Squarelotron UpDownFlip(int ring) {
         System.out.println("Flipping matrix up and down...");
-        int[][] matrixFlipped = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                matrixFlipped[row][col] = matrix[row][col];
-            }
-        }
+        Squarelotron newMatrix = new Squarelotron(size);
+        // int[][] matrixFlipped = new int[size][size];
+        // for (int row = 0; row < size; row++) {
+        //     for (int col = 0; col < size; col++) {
+        //         matrixFlipped[row][col] = matrix[row][col];
+        //     }
+        // }
 
         int sizeR = size-1;
         for (int i = ring -1; i < (size-(ring-1)); i++) {
-            matrixFlipped[ring-1][i] = matrix[sizeR-(ring-1)][i]; //flipping row 1 & row n
-            matrixFlipped[sizeR-(ring-1)][i] = matrix[ring-1][i]; //flipping row n & row 1
-            matrixFlipped[i][ring-1] = matrix[sizeR-i][ring-1]; //flipping col 1
-            matrixFlipped[sizeR-i][ring-1] = matrix[i][ring-1]; //flipping col 1
-            matrixFlipped[i][sizeR-(ring-1)] = matrix[sizeR-i][sizeR-(ring-1)]; //flipping col n
-            matrixFlipped[sizeR-i][sizeR-(ring-1)] = matrix[i][sizeR-(ring-1)]; //flipping col n
+            newMatrix.matrix[ring-1][i] = matrix[sizeR-(ring-1)][i]; //flipping row 1 & row n
+            newMatrix.matrix[sizeR-(ring-1)][i] = matrix[ring-1][i]; //flipping row n & row 1
+            newMatrix.matrix[i][ring-1] = matrix[sizeR-i][ring-1]; //flipping col 1
+            newMatrix.matrix[sizeR-i][ring-1] = matrix[i][ring-1]; //flipping col 1
+            newMatrix.matrix[i][sizeR-(ring-1)] = matrix[sizeR-i][sizeR-(ring-1)]; //flipping col n
+            newMatrix.matrix[sizeR-i][sizeR-(ring-1)] = matrix[i][sizeR-(ring-1)]; //flipping col n
         }
 
-        return matrixFlipped;
+        return newMatrix;
     }
 
-    public int[][] RightTurn(int turns) {
-        System.out.println("Turning matrix...");
-        int[][] matrixTurned = new int[size][size];
+    public void RightTurn(int turns) {
+        System.out.println("Turning matrix " + turns + " times...");
+        // Squarelotron matrixTurned = new Squarelotron(size);
         int sizeR = size-1;
         //int ring = 0;
         if (turns % 4 == 1 || turns % 4 == -3) {
+            Squarelotron matrixTurned = new Squarelotron(size);
             for (int ring = 0; ring <= ((size-size%2)/2); ring++) {
                 for (int i = ring; i < (size-ring); i++) {
 
-                    matrixTurned[i][sizeR-ring] = matrix[ring][i]; //turning row 1
-                    matrixTurned[sizeR-ring][sizeR-i] = matrix[i][sizeR-ring]; //turning col n
-                    matrixTurned[sizeR-i][ring] = matrix[sizeR-ring][sizeR-i]; //turning row n
-                    matrixTurned[ring][i] = matrix[sizeR-i][ring]; //turning col 1
+                    matrix[i][sizeR-ring] = matrixTurned.matrix[ring][i]; //turning row 1
+                    matrix[sizeR-ring][sizeR-i] = matrixTurned.matrix[i][sizeR-ring]; //turning col n
+                    matrix[sizeR-i][ring] = matrixTurned.matrix[sizeR-ring][sizeR-i]; //turning row n
+                    matrix[ring][i] = matrixTurned.matrix[sizeR-i][ring]; //turning col 1
                 }
                 //if (ring == 0){
                 //    break;
                 //}
             }
         } else if (turns % 4 == 2 || turns % 4 == -2) {
+            Squarelotron matrixTurned = new Squarelotron(size);
             for (int ring = 0; ring <= ((size-size%2)/2); ring++) {
                 for (int i = ring; i < (size-ring); i++) {
-                    matrixTurned[ring][i] = matrix[sizeR-ring][sizeR-i]; //turning row n
-                    matrixTurned[i][sizeR-ring] = matrix[sizeR-i][ring]; //turning col 1
-                    matrixTurned[sizeR-ring][sizeR-i] = matrix[ring][i]; //turning row 1
-                    matrixTurned[sizeR-i][ring] = matrix[i][sizeR-ring]; //turning col n
+                    matrix[ring][i] = matrixTurned.matrix[sizeR-ring][sizeR-i]; //turning row n
+                    matrix[i][sizeR-ring] = matrixTurned.matrix[sizeR-i][ring]; //turning col 1
+                    matrix[sizeR-ring][sizeR-i] = matrixTurned.matrix[ring][i]; //turning row 1
+                    matrix[sizeR-i][ring] = matrixTurned.matrix[i][sizeR-ring]; //turning col n
                 }
             }    
         } else if (turns % 4 == 3 || turns % 4 == -1) {
+            Squarelotron matrixTurned = new Squarelotron(size);
             for (int ring = 0; ring <= ((size-size%2)/2); ring++) {
                 for (int i = ring; i < (size-ring); i++) {
-                    matrixTurned[ring][i] = matrix[i][sizeR-ring]; //turning col n
-                    matrixTurned[i][sizeR-ring] = matrix[sizeR-ring][sizeR-i]; //turning row n
-                    matrixTurned[sizeR-ring][sizeR-i] = matrix[sizeR-i][ring]; //turning col 1
-                    matrixTurned[sizeR-i][ring] = matrix[ring][i]; //turning row 1
+                    matrix[ring][i] = matrixTurned.matrix[i][sizeR-ring]; //turning col n
+                    matrix[i][sizeR-ring] = matrixTurned.matrix[sizeR-ring][sizeR-i]; //turning row n
+                    matrix[sizeR-ring][sizeR-i] = matrixTurned.matrix[sizeR-i][ring]; //turning col 1
+                    matrix[sizeR-i][ring] = matrixTurned.matrix[ring][i]; //turning row 1
                 }
             }
-        } else {
-            for (int row = 0; row < size; row++) {
-                for (int col = 0; col < size; col++) {
-                    matrixTurned[row][col] = matrix[row][col];
-                }
-            }
-        }
+        } //else {
+        //     for (int row = 0; row < size; row++) {
+        //         for (int col = 0; col < size; col++) {
+        //             matrixTurned[row][col] = matrix[row][col];
+        //         }
+        //     }
+        // }
 
-        return matrixTurned;
           
     }
 
     public static void main(String[] args) {
-        int size = 4;
+        int size = 3;
 
         Squarelotron testMatrix = new Squarelotron(size);
         for (int row = 0; row < size; row++) {
@@ -117,14 +125,14 @@ public class Squarelotron {
         }
 
 
-        int[][] flipped = testMatrix.UpDownFlip(2);
+        Squarelotron flipped = testMatrix.DiagonalFlip(1);
         for (int row = 0; row < size; row++) {
-            System.out.println(Arrays.toString(flipped[row]));
+            System.out.println(Arrays.toString(flipped.matrix[row]));
         }
         
-        int[][] turned = testMatrix.RightTurn(3);
+        testMatrix.RightTurn(5);
         for (int row = 0; row < size; row++) {
-            System.out.println(Arrays.toString(turned[row]));
+            System.out.println(Arrays.toString(testMatrix.matrix[row]));
         }
     }
 }
